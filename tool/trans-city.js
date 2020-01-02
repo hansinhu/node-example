e7f06d094b9f6e5b8fdf1f8c9b068e633df7201f
hansin <huxinghan@loonxi.com>
2020-01-02 16:35:28 +0800

    update

--- tool/trans-city.js
+++ tool/trans-city.js
@@ -9,12 +9,15 @@ const stateMapObj = stateMap.body.stateCityDTOMap
 const transCityMap = {}
 
 const transStateList = Object.keys(stateMapObj).map((item, i) => {
-  transCityMap[186 + i] = Object.keys(stateMapObj[item])
+  const citys = Object.keys(stateMapObj[item])
+  transCityMap[186 + i] = citys
+  const widthIdItem = stateList.filter(state => state.label === item)
   return {
-    value: 186 + i,
+    value: widthIdItem && widthIdItem[0] ? widthIdItem[0].value : 0,
     label: item,
+    city: citys,
   }
-})
+}).sort((a, b) => a.value - b.value)
 
-console.log(transStateList, transCityMap)
 fs.writeFileSync('./trans-end/city-map.js', `const cistyMapJsonStr = '${JSON.stringify(transCityMap)}'`)
+fs.writeFileSync('./trans-end/in.js', `${JSON.stringify(transStateList)}`)
